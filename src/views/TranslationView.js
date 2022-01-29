271ccd31aa085584e3201719d3bd48a964ce7a6a
oliver.hauck <oliver.hauck@de.experis.com>
2022-01-29 13:19:40 +0100

    FEAT: removes usage of react.CreateElement function and use jsx by iteration instead

--- src/views/TranslationView.js
+++ src/views/TranslationView.js
@@ -1,62 +1,11 @@
 import './TranslationView.css';
-import { createElement } from 'react';
-import ReactDOM from 'react-dom';
+import { useState } from 'react';
 
 const imgPath = `${process.env.PUBLIC_URL}/assets/signs/`;
-
-let text2Translate = "";
 const alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-/* function to store the entered text in variable "text2Translate" */
-const onTextChanged = (event) => {
-  console.log(event.target.value);
-  text2Translate = event.target.value;
-}
-
-/*  function when the user clicked on the "Translate* button 
-    creates one img element per entered char that contains the 
-    respective sign language and
-    renders the resulting array of elements to the <div> element
-    with id "divTranslatedText".
-    Finally it stores the translation text into JSON DB via HTTP PATCH request
-*/
-const OnTranslateClicked = (event) => {
-  console.log("Translate was clicked");
-  const elementArray = [];
-  /* iterate through each character of entered text */
-  for (const char of text2Translate.split("")) {
-    /* if current character is a letter of the alphabet */
-    if(alphabet.indexOf(char.toLowerCase()) > -1) {
-      /* create an <img> element for the respective letter */
-      const img = createElement("img", { 
-        src: `${imgPath}${char.toLowerCase()}.png`,
-        //class: "TranslatedText",
-        style: { width: "50px" },
-        key: elementArray.length
-        }, null);  
-      elementArray.push(img);
-    }
-    else if (char == " ") {
-      const whitespace = createElement("label", { 
-        key: elementArray.length, 
-        style: { 
-          width: "20px",
-          display: "inline-block"
-        }
-      }, null );  
-      elementArray.push(whitespace);
-    }
-  }
-  ReactDOM.render(
-    elementArray, 
-    document.getElementById("divTranslatedText")
-  )
-  storeTranslation(1, text2Translate);
-}
 
 /* start test data */
 const translationHistory = [];
-
 /* end test data */
 
 const storeTranslation = (userId, newTranslation) => {
@@ -93,17 +42,58 @@ const storeTranslation = (userId, newTranslation) => {
   })
 }
 
+let enteredText = "";
+
 function TranslationView() {
+
+  const [ text2Translate, setText2Translate ] = useState("");
+
+  /* function to store the entered text in variable "text2Translate" */
+  const onTextChanged = (event) => {
+    console.log(event.target.value);
+    enteredText = event.target.value;
+    //setText2Translate(event.target.value);
+  }
+  
+  /*  function when the user clicked on the "Translate* button 
+      creates one img element per entered char that contains the 
+      respective sign language and
+      renders the resulting array of elements to the <div> element
+      with id "divTranslatedText".
+      Finally it stores the translation text into JSON DB via HTTP PATCH request
+  */
+  const OnTranslateClicked = (event) => {
+    console.log("Translate was clicked");
+    console.log("enteredText", enteredText);
+    setText2Translate(enteredText);
+    storeTranslation(1, enteredText);
+  }
+
+  const translated = text2Translate.split("").map(
+    (char, index) => {
+      console.log("setting translated...");
+      if(alphabet.indexOf(char.toLowerCase()) > -1) {
+        return (
+          <img key={index} src={`${imgPath}${char.toLowerCase()}.png`} style={ { width: "50px" } } />
+        )
+      }
+      else {
+        return (
+          <label key={ index } style={ {width: "20px", display: "inline-block" }} /> 
+        )
+      }
+    }
+  )
+
   return (
    <>
     <div>
       <input type="text" onChange={onTextChanged} />
       <button onClick={OnTranslateClicked} type="button">Translate</button>
     </div>
-    <div id="divTranslatedText" className="TranslatedText">
-
+    <div className="TranslatedText">
+     { translated }
     </div>
-    <label>&nbsp;</label>
    </> 
   );
 }
