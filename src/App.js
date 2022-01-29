2e147ed6438429af3300dc4bc4983ca38f7a5f86
oliver.hauck <oliver.hauck@de.experis.com>
2022-01-29 06:32:48 +0100

    FEAT: adds functionality to translate entered text into sign language

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import LoginView from "./views/LoginView";
 import TranslationView from "./views/TranslationView";
-import ProfileView from "./views/ProfileViev";
+import ProfileView from "./views/ProfileView";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -13,7 +12,7 @@ function App() {
         <Route path="/" element={<LoginView />}/>
         <Route path="/translation" element={<TranslationView/>}/>
         <Route path="/profile" element={<ProfileView/>}/>
-        <Route path='*' element={<NotFound/>}/>
+        {/* <Route path='*' element={<NotFound/>}/> */}
       </Routes>
     </BrowserRouter>
   </div>
--- src/views/TranslationView.js
+++ src/views/TranslationView.js
@@ -1,8 +1,53 @@
+import './TranslationView.css';
+import { createElement } from 'react';
+import ReactDOM from 'react-dom';
 
+const imgPath = `${process.env.PUBLIC_URL}/assets/signs/`;
+
+let text2Translate = "";
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+/* function to store the entered text in variable "text2Translate" */
+const onTextChanged = (event) => {
+  console.log(event.target.value);
+  text2Translate = event.target.value;
+}
+
+/* function when the user clicked on the "Translate* button */
+const OnTranslateClicked = (event) => {
+  console.log("Translate was clicked");
+  const imgArray = [];
+  /* iterate through each character of entered text */
+  for (const char of text2Translate.split("")) {
+    /* if current character is a letter of the alphabet */
+    if(alphabet.indexOf(char.toLowerCase()) > -1) {
+      /* create an <img> element for the respective letter */
+      const img = createElement("img", { 
+        src: `${imgPath}${char.toLowerCase()}.png`,
+        //class: "TranslatedText",
+        style: { width: "50px" },
+        key: imgArray.length
+        }, null);  
+      imgArray.push(img);
+    }
+  }
+  ReactDOM.render(
+    imgArray, 
+    document.getElementById("divTranslatedText")
+  )
+}
 
 function TranslationView() {
   return (
-   <div></div> 
+   <>
+    <div>
+      <input type="text" onChange={onTextChanged} />
+      <button onClick={OnTranslateClicked} type="button">Translate</button>
+    </div>
+    <div id="divTranslatedText" className="TranslatedText">
+
+    </div>
+   </> 
   );
 }
 
